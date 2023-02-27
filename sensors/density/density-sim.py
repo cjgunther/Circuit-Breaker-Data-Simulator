28c8cdacc4edf27f24b6fc15bced6064f47ee5d0
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-02-26 21:25:47 -0500

    Create Data directories for each sensor

--- sensors/density/density-sim.py
+++ sensors/density/density-sim.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 import calendar
+import os
 
 def generate_density_data(name, month, year):
     # calculate number of days in the given month and year
@@ -25,7 +26,13 @@ def generate_density_data(name, month, year):
     # create DataFrame with timestamp, name, and density columns
     data = pd.DataFrame({'timestamp': date_rng, 'name': name, 'density (lb/ft3)': density_values})
 
-    filename = f"{name}-density-{calendar.month_name[args.month].lower()}-{year}.csv"
+    # create folder for data if it doesn't exist
+    data_dir = 'data'
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    # create filename for CSV file
+    filename = os.path.join(data_dir, f"{name}-density-{calendar.month_name[args.month].lower()}-{year}.csv")
 
     # write DataFrame to CSV file
     data.to_csv(filename, index=False)
